570b3f9f7dc0d9752ebedab421e7e7f503917f68
Mori <mobileabi7@gmail.com>
2022-04-07 08:31:38 +0430

    added programs

--- minimum_area_Cuboid.py
+++ minimum_area_Cuboid.py
@@ -1,13 +1,13 @@
 # minimum area of cuboid , Volume is constant 
 # <=1 v =< 1,000,000
 s = []
-v = float (input("Please enter a Volume of Cuboid : "))
+V = float (input())
 #for minimum of s ----> should x,y,z
 
 for x in range(1,100):
     for y in range(1,100):
         for z in range (1,100):
-            if (x*y*z) == v:
+            if (x*y*z) == V:
                 s.append((2*x*y)+(2*x*z)+(2*y*z))
 print(min(s))
 
--- self_maghloob_number.py
+++ self_maghloob_number.py
@@ -1,10 +1,23 @@
 '''
 برنامه‌ای بنویسید که عدد صحیح n را از ورودی دریافت کند و تعیین کند که آیا این عدد خودمقلوب است یا خیر. عدد خودمقلوب به عددی می گویند که اگر آن را برعکس کنیم، باخودش برابر شود.
 '''
-n= input("pleas enter a number(integer) as n : ")
-x = n[::-1]
+n = int(input())  
+num1 = n
+reversed_num = 0
 
-if n == x :
-    print("Yes")
+while num1 != 0:
+    
+    digit = num1 % 10
+    reversed_num = reversed_num * 10 + digit
+    num1 = num1 // 10
+
+if n == reversed_num:
+    print("YES")
 else:
-    print("No")
+    print("NO")
+
+
+
+ 
+
+
